8efd58a7555e86eac5e391c046c97b6122075df6
Nicolas Earnshaw <nearnshaw@decentraland.org>
2026-01-30 19:07:00 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import {} from '@dcl/sdk/math'
 import { engine } from '@dcl/sdk/ecs'
-import { GltfNodeModifiers, GltfContainer, Transform} from '@dcl/sdk/ecs'
-import {Vector3,} from '@dcl/ecs-math'
+import { setupUi } from './ui'
+
 
 export function main() {
-}
+    // uncomment the line below to initialize UI from ui.tsx
+    //setupUi()
 
+    // your scene code here
+}
